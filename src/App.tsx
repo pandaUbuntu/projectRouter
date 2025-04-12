7a3ae8bb7ceec8a75cd415634d19d2d512dc6c7f
Oleksandr Radionov <fokk190e@gmail.com>
2025-04-12 09:37:32 +0300

    added new form

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,70 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PageLayout from './layouts/page'
 
+type UserDataType = {
+  username: string,
+  email: string,
+  password: string
+}
+
+export function getFormInputValueByName(form: HTMLFormElement, name: string): string {
+  const control = form.elements.namedItem(name) as HTMLInputElement;
+
+  if (!control || control instanceof RadioNodeList || !("value" in control)) {
+      throw new Error(`Form control "${name}" not found or was a RadioNodeList`);
+  }
+
+  return control.value;
+} 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState<UserDataType>({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (event:React.ChangeEvent<HTMLInputElement>) =>{
+    setUserData({
+      ...userData, 
+      [event.target.name]: event.target.value
+    });
+
+    console.log(userData)
+  }
+
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    const userName:string = getFormInputValueByName(event.currentTarget, "username");
+    const email:string = getFormInputValueByName(event.currentTarget, "email");
+    const password:string = getFormInputValueByName(event.currentTarget, "password");
+
+    setUserData({
+      username: userName,
+      email: email,
+      password: password
+    })
+  }
 
   return (
     <PageLayout title="Головна сторінка">
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Введіть ім'я користувача:</label>
+          <input name="username" id="username" type="text" minLength={2} onChange={handleChange} value={userData.username}></input>
+        </div>
+        <div>
+          <label htmlFor="email">Введіть ваш email:</label>
+          <input name="email" id="email" type="email" placeholder='example@gmail.com' onChange={handleChange} value={userData.email}></input>
+        </div>
+        <div>
+          <label htmlFor="password">Введіть ваш пароль:</label>
+          <input name="password" id="password" type="password" minLength={8} maxLength={16} onChange={handleChange} value={userData.password}></input>
+        </div>
+
+        <button type="submit">Саксесс</button>
+     </form>
     </PageLayout >
   )
 }
--- src/pages/page1/index.tsx
+++ src/pages/page1/index.tsx
@@ -1,15 +1,57 @@
 import PageLayout from "../../layouts/page"
+import { getFormInputValueByName } from "../../App";
+import { useState } from "react";
 
-
+type UserDataType = {
+    email: string,
+    password: string
+}
 
 const FirstPage = () =>{
+
+  const [data, setData] = useState<UserDataType>({
+    email: "",
+    password: ""
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      let email = getFormInputValueByName(event.currentTarget, "email");
+      let password = getFormInputValueByName(event.currentTarget, "password");
+
+      // Валідації email/password
+
+      return {
+        email: email,
+        password: password
+      }
+  }
+
+  const handleChange = (event:React.ChangeEvent<HTMLInputElement>) =>{
+      event.preventDefault();
+      setData({
+        ...data, 
+        [event.target.name]: event.target.value
+      });
+  
+      console.log(data)
+  }
 
 
   return (
     <PageLayout title="Моя перша сторінка">
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi nostrum exercitationem unde, facere voluptatibus id! Officia cupiditate nemo molestias nesciunt doloribus voluptatem odio minus nulla vero asperiores. Quibusdam, dignissimos obcaecati!
-        </p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Введіть email: </label>
+          <input name="email" type="email" onChange={handleChange} value={data.email}/>
+        </div>
+        <div>
+          <label htmlFor="password">Введіть пароль: </label>
+          <input name="password" type="password" minLength={8} maxLength={16} onChange={handleChange} value={data.password} />
+        </div>
+        <button type="submit">Логін</button>
+      </form>
     </PageLayout>
   )
 }
